0940a29e28b100fc665085de495aeca96708ca31
Nicolas Broca <n.broca@gmail.com>
2024-05-30 21:43:09 +1000

    Add installation script for linux (#1)

---
+++ apply-config-linux.sh
@@ -0,0 +1,57 @@
+#!/user/bin/env bash
+
+sudo apt update
+
+# install dependencies
+## antigen
+curl -L git.io/antigen > ~/antigen.sh
+
+## install eza
+sudo apt update
+sudo apt install -y gpg
+
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+
+## install zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+## install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+## install thefuck
+sudo apt update
+sudo apt --yes install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
+
+## install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+## install the rest
+sudo apt --yes install python3.11 \
+    python3.11-venv \
+    fzf \
+    bat \
+    zsh
+
+# install nvm (node is a dependency of neovim's LSP
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash  
+
+# get config
+echo ".cfg" >> .gitignore
+git clone --bare https://github.com/DrKabum/dotfiles.git $HOME/.cfg
+/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+
+# make zsh default
+chsh -s $(which zsh)
+
+echo "installation done. Please log out and log back in."
